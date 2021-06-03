dc9ec63b1b0c866337b8733569a42ce3beb24444
tdudkowski <tdudkowski@gmail.com>
2021-06-03 20:38:47 +0200

    add news on June event update Gatsby to 3.6

--- src/pages/news.js
+++ src/pages/news.js
@@ -13,13 +13,14 @@ const NewsPage = ({ data }) => (
 
     <p>Szanowni Państwo, ruszamy z nowym projektem pt. "Z Wrocławiem w tle", który obejmować będzie cztery spotkania literackie z wrocławskimi autorami oraz cztery wieczorne wycieczki tematyczne. Spotkania odbywać się będą w nowym Odra Centrum stojącym przy moście Grunwaldzkim. Spotkania i wycieczki są bezpłatne, obowiązują zapisy.</p>
     <p>Cykl poprowadzi: Małgorzata Urlich-Kornacka</p>
-    <p>Wrocław jest bez wątpienia miastem literatury i ma się czym poszczycić. Nie przez przypadek został Światową Stolicą Książki UNESCO, a wiersz poety Tadeusza Różewicza stał się w 2016 roku oficjalnym hymnem i od tamtego czasu również oficjalną muzyczną wizytówką każdej kolejnej Światowej Stolicy Książki UNESCO. Liczne wydarzenia literackie skłoniły nas do przygotowania własnego projektu łączącego spotkania z autorami z tematycznymi wycieczkami i promocją niezwykłego miejsca na mapie Wrocławia – nowego Odra Centrum.</p>
+    <p>Wrocław jest bez wątpienia miastem literatury i ma się czym poszczycić. Nie przez przypadek został Światową Stolicą Książki UNESCO, a wiersz poety Tadeusza Różewicza stał się w 2016 roku oficjalnym hymnem i od tamtego czasu również oficjalną muzyczną wizytówką każdej kolejnej Światowej Stolicy Książki UNESCO.</p>
+    <p>Liczne wydarzenia literackie skłoniły nas do przygotowania własnego projektu łączącego spotkania z autorami z tematycznymi wycieczkami i promocją niezwykłego miejsca na mapie Wrocławia – nowego Odra Centrum.</p>
 
-    <p>20-21 maja</p>
+    <p>24-25 czerwca</p>
 
     <ul>
-      <li>20 maja, godz. 16.00; spotkanie z autorką Malwiną Ferenz - Odra Centrum.</li>
-      <li>21 maja, godz. 17.00; spacer "Wrocław z dreszczykiem".</li>
+      <li>24 czerwca, godz. 16.00; spotkanie z wrocławską autorką Jolantą Marią Kaletą - Odra Centrum.</li>
+      <li>25 czerwca, godz. 17.00; spacer "Złoto Wrocławia. Między faktami a fikcją literacką".</li>
     </ul>
 
     <section className="videos">
@@ -37,8 +38,10 @@ const NewsPage = ({ data }) => (
     </ul>
 
     <div>
-      <Img fixed={data.afisz_maj.childImageSharp.fixed} />
+      <Img fixed={data.afisz_czerwiec.childImageSharp.fixed} />
       <hr />
+      <Img fixed={data.afisz_maj.childImageSharp.fixed} />
+
       <Img fixed={data.afisz_luty.childImageSharp.fixed} />
     </div>
 
@@ -50,6 +53,15 @@ export default NewsPage
 
 export const indexQuery = graphql`
   query NewsImages {
+    afisz_czerwiec: file(
+      relativePath: {eq: "afisz-zwrowtle_czerwiec_x960.jpg" }
+    ) {
+            childImageSharp {
+        fixed(width: 640) {
+        ...GatsbyImageSharpFixed_withWebp
+      }
+      }
+    }
         afisz_maj: file(
       relativePath: {eq: "afisz-z_wroclawiem_w_tle-maj.jpg" }
     ) {
